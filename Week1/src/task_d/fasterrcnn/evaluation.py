beb53b95afc075ed433f6fdd49a26f85a0e87d0b
loreoregi <loreoregi@gmail.com>
2026-02-21 12:00:53 +0100

    add main_evaluation orchestrator to trigger model-specific evaluations

--- Week1/src/task_d/fasterrcnn/evaluation.py
+++ Week1/src/task_d/fasterrcnn/evaluation.py
@@ -52,12 +52,3 @@ def evaluation(args):
 
     wandb.finish()
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--model", type=str, default="fasterrcnn")
-    parser.add_argument("--variant", type=str, default="resnet50_fpn_v2")
-    parser.add_argument("--batch_size", type=int, default=8)
-    
-    args = parser.parse_args()
-    evaluation(args)
-
