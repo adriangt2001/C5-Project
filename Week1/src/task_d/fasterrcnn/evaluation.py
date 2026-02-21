f0060df92ce8ae2effaea62fa02f01f51478f03d
loreoregi <loreoregi@gmail.com>
2026-02-21 11:27:36 +0100

    Evaluation (task_d) for fasterrcnn-torchvision

---
+++ Week1/src/task_d/fasterrcnn/evaluation.py
@@ -0,0 +1,63 @@
+import torch
+import wandb
+import argparse
+from tqdm import tqdm
+from torchmetrics.detection.mean_ap import MeanAveragePrecision
+from torch.utils.data import DataLoader
+from src.models import FasterRCNN
+from src.custom_datasets import KittiDatasetTorchvision
+
+def evaluation(args):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    wandb.init(
+        project="C5-Week1", 
+        entity="c5-team2", 
+        name=f"Eval-{args.model}-{args.variant}",
+        config=args
+    )
+
+    ds = KittiDatasetTorchvision('dataset/KITTI-MOTS', 'instances_txt', 'training', 'src/custom_datasets/val.seqmap')
+    loader = DataLoader(ds, batch_size=args.batch_size, collate_fn=lambda x: tuple(zip(*x)))
+    detector = FasterRCNN(variant=args.variant, device=device)
+    # class_metrics=True to see mAP for Car and Pedestrian separately
+    metric = MeanAveragePrecision(box_format='xyxy', class_metrics=True)
+
+    for images, targets in tqdm(loader, desc="Evaluating"):
+        detector.evaluate(images, targets, metric)
+    
+    print("Computing final metrics...")
+    results = metric.compute()
+
+    metrics_to_log = {
+        "mAP/main": results["map"],          # AP @.50:.95
+        "mAP/50": results["map_50"],         # AP @.50
+        "mAP/75": results["map_75"],         # AP @.75
+        "mAP/small": results["map_small"],
+        "mAP/medium": results["map_medium"],
+        "mAP/large": results["map_large"],
+    }
+
+    if "map_per_class" in results:
+        # 1: Car, 2: Pedestrian
+        metrics_to_log["mAP/class_Car"] = results["map_per_class"][0]
+        metrics_to_log["mAP/class_Pedestrian"] = results["map_per_class"][1]
+    
+    wandb.log(metrics_to_log)
+
+    print(f"\nEvaluation Finished for {args.variant}:")
+    print(f"mAP @.50:.95: {results['map']:.4f}")
+    print(f"mAP @.50:      {results['map_50']:.4f}")
+    print(f"mAP @.75:      {results['map_75']:.4f}")
+
+    wandb.finish()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, default="fasterrcnn")
+    parser.add_argument("--variant", type=str, default="resnet50_fpn_v2")
+    parser.add_argument("--batch_size", type=int, default=8)
+    
+    args = parser.parse_args()
+    evaluation(args)
+
